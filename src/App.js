c55fb01c6cf684b0fa07ab55eee24707f9b36de5
mihyunLee <mihy621@gmail.com>
2022-08-24 22:13:33 +0900

    feat: 컴포넌트 추가, 프로젝트 UI 완성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import React from "react";
+import { createGlobalStyle } from "styled-components";
+import TodoCreate from "./components/TodoCreate";
+import TodoHead from "./components/TodoHead";
+import TodoList from "./components/TodoList";
+import TodoTemplate from "./components/TodoTemplate";
+
+const GlobalStyle = createGlobalStyle`
+  body{
+    background: #e9ecef;
+  }
+`;
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <GlobalStyle />
+      <TodoTemplate>
+        <TodoHead />
+        <TodoList />
+        <TodoCreate />
+      </TodoTemplate>
+    </>
+  );
 }
 
 export default App;
